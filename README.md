ec34be5a2118ee08fc438882471a0b0b6d3839c1
Crossover661 <sanjay.k.srikanth@gmail.com>
2025-04-24 16:34:09 -0700

    push all files, not just in chain folder

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # graph-and-chain
 Graph and chain generation for Quantifying Gerrymandering project.
+
+**'graph' folder**:
+This folder contains the dual graph of the shapefile and the code needed to generate it. Running "generate-dual-graph.py" will create "dual-graph.json" - a file showing the borders between California census block groups. Some borders are added and removed to take into account land connections, bridges, and ferries.
+
+**'data' folder**:
+This folder contains the shapefiles, population, and election data for 2020 census block groups (CBGs) in California.
+
+"tl_2020_06_bg20" folder contains the TIGER/Line shapefile for 2020 California CBGs (.shp), its shape index (.shx), projection (.prj), attributes (.dbf), and the code page for the attributes (.cpg).
+
+"cacbg20" contains a shapefile containing all non-water CBGs in California, its prison-adjusted population as of the 2020 census, the population of each ethnic group (Non-Hispanic White, Hispanic/Latino, Black, Asian, Native American, Pacific Islander), and the results of the 2020 presidential election and 2022 gubernatorial election in each CBG. 
+
+The populations and 2020 election results are sourced from Dave's Redistricting. 2022 election results are sourced from the "2022_ca_gov" repository in this GitHub organization, which was synthesized through data from the Statewide Database. The file is in a format that can be uploaded to Dave's Redistricting as a custom election dataset.
+
+**'chain' folder**:
+This folder contains the code used to generate random districtings and evaluate their "expected" partisan distribution, which is used to evaluate whether a district is gerrymandered. If a districting plan contains an abnormally large number of Democratic or Republican districts that cannot be explained by random chance, the plan is considered gerrymandered.
--- graph/generate-dual-graph.py
+++ graph/generate-dual-graph.py
@@ -6,11 +6,9 @@ import networkx as nx
 import matplotlib.pyplot as plt
 
 root = (Path(__file__).parent).parent
-shapefile_path = root / "data" / "ca-cbgs-graphing.zip"
-gdf = gpd.read_file(shapefile_path) # synced shapefile generated using script from sync-cbg-shape-with-voting repo
+shapefile_path = root / "data" / "cacbg20" / "cacbg20.shp"
+gdf = gpd.read_file(shapefile_path) # shapefile with population and election data as attributes
 
-
-# graph = Graph.from_geodataframe(gdf, adjacency="queen")
 graph = Graph.from_geodataframe(gdf, ignore_errors=False)
 
 print(len(graph.nodes)) # 25584 CBGs that are non-water (of the 25607 CBGs total in California)
@@ -20,29 +18,32 @@ def add_edge(b1, b2):
     b2_index = gdf.index[gdf["GEOID20"] == b2].to_list()[0]
     graph.add_edges_from([(b1_index, b2_index)])
 
-add_edge("060759804011", "060750604002") # Farallon Islands - mainland San Francisco
-add_edge("060750615072", "060750179031") # Treasure Island - mainland San Francisco (Bay Bridge)
-add_edge("060014017003", "060750179031") # Treasure Island - Oakland (Bay Bridge)
-add_edge("060750179032", "060750101011") # Alcatraz Island - mainland San Francisco
-add_edge("060750601002", "060411311001") # Golden Gate Bridge (San Francisco - Marin County)
-add_edge("060014061003", "060014271001") # Alameda - Oakland
-add_edge("060014061002", "060014271003") # Alameda - Oakland
-add_edge("060133570001", "060952506042") # Carquinez Strait (Crockett - Vallejo)
-add_edge("060133200011", "060952521021") # Benicia - Martinez bridge
-add_edge("060411122022", "060133780001") # Richmond - San Rafael bridge
-add_edge("060816082002", "060014371011") # San Mateo - Hayward bridge
-add_edge("060816118001", "060014443031") # Dunbarton bridge
-add_edge("060839801001", "061110025003") # Santa Cruz Island - Ventura (connected by ferry)""
-add_edge("061110036181", "061110036183") # Anacapa Island - Oxnard (connected by ferry)
-add_edge("061119800001", "061110036183") # San Nicolas Island - mainland
-add_edge("060375990001", "060375760011") # Santa Catalina Island - Long Beach (connected by ferry)
-add_edge("060375991001", "060375991002") # San Clemente Island - Santa Catalina Island
-add_edge("060590995145", "060590995144") # Trinidad Island, Huntington Beach
-add_edge("060590995143", "060590995141") # Huntington Beach
-add_edge("060590629001", "060590635001") # Newport Beach
-add_edge("060590630062", "060590630051") # Newport Beach
-add_edge("060730109001", "060730050001") # Coronado - San Diego
+def remove_edge(b1, b2):
+    b1_index = gdf.index[gdf["GEOID20"] == b1].to_list()[0]
+    b2_index = gdf.index[gdf["GEOID20"] == b2].to_list()[0]
+    graph.remove_edges_from([(b1_index, b2_index)])
 
+# Edges to remove
+remove_edge("060750179031", "060411242003") # due to San Francisco's boundaries, CBG 060750179031 is not contiguous 
+remove_edge("060750179031", "060014287002") # due to San Francisco's boundaries, CBG 060750179031 is not contiguous
+remove_edge("060750601002", "060411302041") # two CBGs in the bay area that border each other on water only
+remove_edge("060730099021", "060730113001") # San Diego - Coronado (not on bridge)
+remove_edge("060730099021", "060730111001") # San Diego - Coronado (not on bridge)
+
+
+# Edges to add
+add_edge("060750179031", "060750615072") # Treasure Island - San Francisco
+add_edge("060750179031", "060014017003") # Treasure Island - Oakland
+add_edge("060750179032", "060750101011") # Alcatraz Island - San Francisco
+add_edge("060759804011", "060759803001") # Farallon Islands - San Francisco
+add_edge("060839801001", "061110025003") # Santa Cruz Island - Ventura (connected by ferry)
+add_edge("061110036181", "061110036183") # Anacapa Island - Oxnard (connected by ferry)
+add_edge("061119800001", "060839801001") # San Nicolas Island - Santa Barbara Island
+add_edge("060839801001", "061110036183") # Santa Barbara Island - Oxnard (connected by ferry)
+add_edge("060375991001", "060375991002") # San Clemente Island - Santa Catalina Island
+add_edge("060375991002", "060379800311") # Santa Catalina Island - San Pedro, Los Angeles
+add_edge("060375991002", "060375760011") # Santa Catalina Island - Long Beach
+add_edge("060375991002", "060590628001") # Santa Catalina Island - Newport Beach
 
 graph_filepath = root / "graph" / "dual-graph.json"
 graph.to_json(graph_filepath)
